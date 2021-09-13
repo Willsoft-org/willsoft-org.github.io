c5cf17c74761ae9cf75667f544ad2ef5dd2394d2
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2021-09-14 00:22:07 +0200

    Add Google Analytics

--- src/scripts.php
+++ src/scripts.php
@@ -1,3 +1,16 @@
 <script src="scripts/cookies.js"></script>
 <script src="scripts/hamburger.js"></script>
 <script src="scripts/typewriter.js"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NQ7H5WKFBM"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+	dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-NQ7H5WKFBM');
+</script>
